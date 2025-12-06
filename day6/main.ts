535e028b08e49a6e26b013fe0ed64dce2ba74858
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2025-12-06 20:08:27 +0100

    cleanup day6

--- day6/main.ts
+++ day6/main.ts
@@ -1,8 +1,6 @@
 import { getInputLines } from "../lib/util.ts";
 
 type Op = "+" | "*";
-type Cell = string;
-type Column = Cell[];
 
 async function main() {
     const lines = await getInputLines();
@@ -70,71 +68,6 @@ async function main() {
     }
 
     console.log(`Grand total is ${totalAnswer} of answers' summed`);
-
-    // const columns: Column[] = [];
-
-    // const insert = (x: number, val: Cell) => {
-    //     if (!columns[x]) {
-    //         columns[x] = [];
-    //     }
-    //     columns[x].push(val);
-    // };
-
-    // for (const line of lines) {
-    //     const cols = line.split(/\s+/);
-    //     for (let x = 0; x < cols.length; x++) {
-    //         const cell = cols[x];
-    //         switch (cell) {
-    //             case "*": {
-    //                 insert(x, { op: cell });
-    //                 break;
-    //             }
-    //             default: {
-    //                 insert(x, { n: cell });
-    //                 // const n = Number.parseInt(cell);
-    //                 // if (!Number.isFinite(n)) {
-    //                 //     throw new Error(`Expected number at col ${x}: ${cell}`);
-    //                 // }
-    //                 // insert(x, { n });
-    //             }
-    //         }
-    //     }
-    // }
-
-    // let answerTotal = 0;
-    // for (const col of columns) {
-    //     const answer = calcColumn(col);
-    //     answerTotal += answer;
-    // }
-    //
-    // console.log(`Grand total is ${answerTotal} of all columns' answers summed`);
-}
-
-function calcColumn(col: Column): number {
-    let op: Op | null = null;
-    const nums: number[] = [];
-    for (const cell of col) {
-        if ("n" in cell) {
-            nums.push(cell.n);
-            continue;
-        }
-        op = cell.op;
-    }
-
-    switch (op) {
-        case null: {
-            throw new Error("Expected operator (+/*) in column");
-        }
-        case "+": {
-            return nums.reduce((sum, n) => sum + n, 0);
-        }
-        case "*": {
-            return nums.reduce((sum, n) => sum * n, 1);
-        }
-        default: {
-            throw new Error("unreachable");
-        }
-    }
 }
 
 main();
